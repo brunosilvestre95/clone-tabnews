b256a33da7529e1d05d4a0a9179384efc0ca31e2
bruno silvestre <153228287+brunosilvestre95@users.noreply.github.com>
2025-09-29 23:56:49 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1> Monique, eu te amo. Obrigado por existir e sempre me apoiar! </h1>
+    return <h1> Monique, eu te amo. Obrigado por existir e sempre me apoiar! ♥ </h1>
 }
 
 export default Home;
