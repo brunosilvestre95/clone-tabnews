1bedaa220bcf07bcc284e0e4c1b6092ca854e09d
bruno silvestre <153228287+brunosilvestre95@users.noreply.github.com>
2025-10-01 14:21:42 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home(){
-    return <h1> Monique, eu te amo. Obrigado por existir e sempre me apoiar! ♥ </h1>
+function Home() {
+  return (
+    <h1> Monique, eu te amo. Obrigado por existir e sempre me apoiar! ♥ </h1>
+  );
 }
 
 export default Home;
